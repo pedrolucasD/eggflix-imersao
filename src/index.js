617e87e1ba89b608e38d8e2bdc13a0dacb7da4d0
Pedro Soares <pedro.soares@systemhope.com.br>
2020-08-05 18:44:52 -0300

    primeira aula projeto Imersao React Alura

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Home from './pages/Home';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/video';
+import CadastroCategoria from './pages/cadastro/categoria';
+
+const Pagina404 = () => (
+ <div>
+   Erro 404 Página não encontrada
+ </div>
+)
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
